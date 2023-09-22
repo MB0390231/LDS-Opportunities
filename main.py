2aa2f8b984e10236de037b97693de7821d70fe86
Michael <93745624+MB0390231@users.noreply.github.com>
2023-09-22 14:00:45 -0500

    import error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from oauth_flask.utils import update_lds_opportunities
+from gohighlevel_oauth_demo_flask.utils import update_lds_opportunities
 import gspread
-from oauth_flask.keys import GoogConfig
+from gohighlevel_oauth_demo_flask.keys import GoogConfig
 
 if __name__ == "__main__":
     GC = gspread.service_account_from_dict(GoogConfig.CREDENTIALS)
